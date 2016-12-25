ec191ff76360a7893c8178c73924a4603157aeb2
Kayla Andersen <xctrumpet23@users.noreply.github.com>
2016-12-25 13:47:04 -0500

    Add files via upload

---
+++ BalloonRobotServoTest.ino
@@ -0,0 +1,24 @@
+// Test program for controlling servos with the tinyduino
+// for Balloon Robot 
+
+#include <Servo.h> 
+
+Servo rightLeg;
+Servo leftLeg;
+
+void setup() {
+  // put your setup code here, to run once:
+  rightLeg.attach(9);
+  //leftLeg.attach(10);
+  rightLeg.write(0);
+  //leftLeg.write(0);
+}
+
+void loop() {
+ rightLeg.write(180);
+ delay(1000);
+ rightLeg.write(0);
+ delay(1000);
+ 
+
+}
